c9bbddc5a067c7163e97cba18c875ab11163c087
Minh3132004 <vubinhminh04@gmail.com>
2026-05-03 15:27:37 +0700

    Thêm trang Flash Sale và Best Seller vào routing (fix merge)

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -4,6 +4,8 @@ import Footer from "../components/layout/footer/Footer";
 import HomePage from "../components/homepage/HomePage";
 import { Route, Routes } from "react-router-dom";
 import ActiveAccountPage from "../pages/ActiveAccountPage";
+import BestSellerPage from "../pages/bestseller/BestSellerPage";
+import FlashSalePage from "../pages/flashsale/FlashSalePage";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <main style={{ flex: 1 }}>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/flash-sale" element={<FlashSalePage />} />
+          <Route path="/best-sellers" element={<BestSellerPage />} />
           <Route path="/active/:email/:activationCode" element={<ActiveAccountPage />} />
         </Routes>
       </main>
